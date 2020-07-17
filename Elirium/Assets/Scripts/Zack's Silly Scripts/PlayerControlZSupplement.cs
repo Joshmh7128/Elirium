10c02bb46eb403381650c220001e250df29a9f4a
Zachary Anderson <studiousstudent2019@gmail.com>
2020-07-16 21:11:30 -0400

    Player Controller Rework Begun
    I done started remaking the character controller. It isn't too different, but I'm using the CharacterController component instead of a rigidbody. This takes care of some problems like walking up slopes, and it isn't too hard to implement features that came naturally with rigidbodies.
    There's still more work to do before I'm satisfied with the controller, and it also needs some fine tuning so that the physics feel good.

---
+++ Elirium/Assets/Scripts/Zack's Silly Scripts/PlayerControlZSupplement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlZSupplement : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    private Vector3 momentum;
+    private Vector3 move;
+
+    private float theta;
+
+    private float interpolation = 1f;
+
+    private void Awake()
+    {
+        transform.Rotate(Vector3.up * playerTransform.eulerAngles.y * -1);
+    }
+
+    public Vector3 Thang(float angle, float x, float z)
+    {
+        theta = angle * Mathf.Deg2Rad;
+
+        move = (transform.right * (x * Mathf.Cos(theta) + z * Mathf.Sin(theta))) + (transform.forward * (z * Mathf.Cos(theta) + x * Mathf.Cos(theta + Mathf.PI / 2)));
+
+        momentum = Vector3.Lerp(momentum, move, interpolation);
+        
+        return momentum;
+    }
+}
