a1f8a8071080e3436ffc0ef2d0b5748592478570
Zachary Anderson <studiousstudent2019@gmail.com>
2020-09-22 20:34:46 -0400

    One Last Commit
    I don't think there's gonna be any more commits here, since we're moving to Unity Collab or sumthing. But here's the navmesh stuff. It's cool

---
+++ Elirium/Assets/Zack's Things/Peepo Explores Elirium/Peepo Scripts/PeepoExploresTest.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PeepoExploresTest : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform ball;
+    public Transform player;
+
+    private Collider _heldCollider;
+    private Orb_PuzzleScript _heldOrb;
+
+    private GameObject holdPointPeepo;
+
+    private Vector3 origin;
+
+    private bool isHolding = false;
+
+    private void Start()
+    {
+        holdPointPeepo = GameObject.Find("Hold Point Peepo");
+        origin = transform.position;
+    }
+
+    void Update()
+    {
+        if (!isHolding)
+        {
+            agent.stoppingDistance = 0;
+            agent.SetDestination(ball.position);
+        }
+        else
+        {
+            agent.stoppingDistance = 5;
+            agent.SetDestination(player.position);
+        }
+
+        if (_heldOrb != null && !isHolding)
+        {
+            if (!_heldOrb.isHeld)
+            {
+                _heldOrb.holdPoint = holdPointPeepo.transform;
+                _heldOrb.isHeld = true;
+                isHolding = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Orb_PuzzleScript>())
+        {
+            _heldCollider = other;
+            _heldOrb = other.GetComponent<Orb_PuzzleScript>();
+
+            if (!_heldOrb.isHeld)
+            {
+                _heldOrb.holdPoint = holdPointPeepo.transform;
+                _heldOrb.isHeld = true;
+                isHolding = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (Collider.ReferenceEquals(other, _heldCollider))
+        {
+            isHolding = false;
+            _heldCollider = null;
+            _heldOrb = null;
+        }
+    }
+}
